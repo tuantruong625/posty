1c3299b31c610174324907d402aac8a5f5e2a0fe
Tuan N. Truong <tuan.truong625@gmail.com>
2021-03-09 16:19:02 -0500

    Add likes/unlike

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -34,6 +34,23 @@
        <a href="" class="font-bold">{{ $post->user->name }}</a>
        <span class="text-sm">{{ $post->created_at->diffForHumans() }}</span>
        <p class="mv-2">{{$post->body}}</p>
+
+       <div class="flex items-center">
+        @if(!$post->likedBy(auth()->user()))
+          <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+            @csrf
+            <button type="submit" class="text-blue-500">Like</button>
+          </form>
+        @else
+          <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+            @csrf
+            @method('DELETE')
+            <button type="submit" class="text-blue-500">Unlike</button>
+          </form>
+        @endif
+
+        <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+       </div> 
       </div>
      @endforeach
 
