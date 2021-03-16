d4cf4524ab99b98c1240158150d0b335301b885a
Tuan N. Truong <tuan.truong625@gmail.com>
2021-03-15 22:03:27 -0400

    Add auth guards

--- app/Http/Controllers/PostLikeController.php
+++ app/Http/Controllers/PostLikeController.php
@@ -23,9 +23,11 @@ class PostLikeController extends Controller
         $post->likes()->create([
             'user_id' => $request->user()->id,
         ]);
-    
-        Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
 
+        if(!$post->likes()->onlyTrashed()->where('user_id', $request->user()->id)->count()){
+            Mail::to($post->user)->send(new PostLiked(auth()->user(), $post));
+        }
+        
         return back();
     }
 
